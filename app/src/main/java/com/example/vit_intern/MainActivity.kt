f139a26360b4e3243f76b2da8622d8b510ddf16c
yashwanth1239 <yashwanthkumarvasireddy@gmail.com>
2024-06-08 11:26:27 +0530

    activities and log entries

--- app/src/main/java/com/example/vit_intern/MainActivity.kt
+++ app/src/main/java/com/example/vit_intern/MainActivity.kt
@@ -1,6 +1,10 @@
 package com.example.vit_intern
 
+import android.content.Intent
+import android.net.Uri
 import android.os.Bundle
+import android.util.Log
+import android.view.View
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
@@ -17,4 +21,12 @@ class MainActivity : AppCompatActivity() {
             insets
         }
     }
+
+    fun clickHandler(view: View) {
+        Log.i("MainActivity", "clickHandler: button clicked ")
+        var dialIntent: Intent= Intent(Intent.ACTION_DIAL)
+        startActivity(dialIntent)
+    }
+
+
 }
