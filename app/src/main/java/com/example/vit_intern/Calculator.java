4d19f8c43c82ae2f22de914b7f82a53694658f51
yashwanth1239 <yashwanthkumarvasireddy@gmail.com>
2024-06-13 10:48:45 +0530

    unit test cases

---
+++ app/src/main/java/com/example/vit_intern/Calculator.java
@@ -0,0 +1,12 @@
+package com.example.vit_intern;
+
+public class Calculator {
+
+    public int add(int a,int b){
+        return a+b;
+    }
+    public int mul(int a,int b){
+        return a*b;
+    }
+
+}
